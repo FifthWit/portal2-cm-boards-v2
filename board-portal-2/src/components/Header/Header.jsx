780f88f4773040422cad9e764e4e2ef5811478f9
FifthWit <fifthwitbusiness@gmail.com>
2024-10-23 00:08:15 -0500

    added header stuff, a lot to go but thats fine

--- board-portal-2/src/components/Header/Header.jsx
+++ board-portal-2/src/components/Header/Header.jsx
@@ -9,13 +9,15 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  Button,
+  // Button,
   Switch
 } from "@material-ui/core"
 import { BrowserRouter, Link } from "react-router-dom"
 import { useStyles, CustomButton } from "./style.js"
 import Dropdown from "./Dropdown"
+import { ModeToggle } from "@/components/mode-toggle"
 import React from "react"
+import { Button, buttonVariants } from "@/components/ui/button"
 /**
  * @name - Header
  * @desc - Contains the toolbar for header. Displays the website title. Shows links for home page,
@@ -27,87 +29,79 @@ import React from "react"
  * @param -
  * @return -
  */
-const Header = ({ handleChange, themeStatus }) => {
-  const classes = useStyles()
 
+// Data for buttons
+const buttonData = [
+  {
+    variant: 'text',
+    href: '/',
+    src: scoreUpdates,
+    text: 'Score Updates'
+  },
+  {
+    variant: 'text',
+    href: '/sp',
+    src: singlplayerIcon,
+    text: 'Single Player'
+  },
+  {
+    variant: 'text',
+    href: '/coop',
+    src: coopIcon,
+    text: 'Cooperative'
+  },
+  {
+    variant: 'text',
+    href: '/agg-selector',
+    src: aggregatedIcon,
+    text: 'Aggregated'
+  },
+  {
+    variant: 'text',
+    href: 'https://www.speedrun.com/Portal_2',
+    src: fullGameRunsIcon,
+    text: 'Full Game Runs'
+  },
+  {
+    variant: 'text',
+    href: 'https://steamcommunity.com/openid/login?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=http%3A%2F%2Fboard.iverb.me%2Flogin&openid.realm=http%3A%2F%2Fboard.iverb.me&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select',
+    src: steamIcon,
+    text: 'Sign In'
+  }
+];
+
+const HeaderButton = ({ variant = "default", href, src, text, iconClassName }) => {
   return (
-    <div id='container' className={classes.root}>
-      <AppBar color='primary' position='static'>
-        <Toolbar className={classes.toolbar}>
-          <Grid
-            container
-            justify='space-around'
-            alignItems='flex-start'
-            direction='column'>
-            <Grid item>
-              <Typography
-                color='textPrimary'
-                className={`text-red-500 ${classes.title}`}
-                noWrap
-                variant='h5'
-                >
-                Portal 2 Leaderboards
-              </Typography>
-            </Grid>
-            <Grid item className={classes.headerLinks}>
-              <CustomButton variant='text' href='/'>
-                <img src={scoreUpdates} className={classes.icon} />
-                Score Updates
-              </CustomButton>
-              <CustomButton variant='text' href='/sp'>
-                <img src={singlplayerIcon} className={classes.icon} />
-                Single Player
-              </CustomButton>
-              <CustomButton variant='text' href='/coop'>
-                <img src={coopIcon} className={classes.icon} />
-                Cooperative
-              </CustomButton>
-              <CustomButton variant='text' href='/agg-selector'>
-                <img src={aggregatedIcon} className={classes.icon} />
-                Aggregated
-              </CustomButton>
-              {/* <Dropdown /> */}
-              <CustomButton
-                variant='text'
-                href='https://www.speedrun.com/Portal_2'>
-                <img src={fullGameRunsIcon} className={classes.icon} />
-                Full Game Runs
-              </CustomButton>
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            direction='column'
-            justify='center'
-            alignItems='flex-end'
-            xs='2'>
-            <Grid item container justify='flex-end'>
-              <CustomButton
-                className={classes.steam}
-                href='https://steamcommunity.com/openid/login?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=http%3A%2F%2Fboard.iverb.me%2Flogin&openid.realm=http%3A%2F%2Fboard.iverb.me&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select'
-                id='steam-login'>
-                <div>Sign In</div>
-                <img src={steamIcon} style={{ height: "45px" }} />
-              </CustomButton>
-            </Grid>
-            <Grid item container justify='flex-end' alignItems='center'>
-              <Grid item>
-                <Typography color='textPrimary'>Dark</Typography>
-              </Grid>
-              <Switch
-                color='default'
-                checked={themeStatus}
-                onChange={handleChange}
-              />
-              <Grid>
-                <Typography color='textPrimary'>Light</Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </div>
+      <a href={href} className="flex flex-row items-center space-x-2 m-2 hover:bg-opacity-10 bg-white bg-opacity-0 duration-150 transition-all rounded-lg p-4">
+        <img src={src} alt={text} className={`${iconClassName} aspect-square h-8`} />
+        <span className="hidden sm:inline">{text}</span>
+      </a>
   )
 }
 
-export default Header
+const Header = ({ handleChange, themeStatus }) => {
+  const classes = useStyles();
+
+  return (
+    <div className="bg-secondary text-white flex justify-between items-center px-4">
+      <div className="flex items-center space-x-4 font-semibold">
+        <h5>
+          Portal 2 Leaderboards
+        </h5>
+        {buttonData.map((button, index) => (
+          <HeaderButton
+            key={index}
+            variant={button.variant}
+            href={button.href}
+            src={button.src}
+            text={button.text}
+          />
+        ))}
+      </div>
+      
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Header;
